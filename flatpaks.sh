e12de599492250f01d2d754cf69f3ad0b0ff3e12
kaleb <cardtunic@gmail.com>
2025-01-06 20:57:39 -0300

    feat: add beekeeper studio flatpak

--- flatpaks.sh
+++ flatpaks.sh
@@ -1,5 +1,4 @@
 flatpak install flathub com.visualstudio.code
 flatpak install flathub com.google.Chrome
 flatpak install flathub com.spotify.Client
-
-alias code="flatpak run com.visualstudio.code"
+flatpak install flathub io.beekeeperstudio.Studio
